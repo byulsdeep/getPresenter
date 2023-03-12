603db5451f8b074d44ab8abef4713c0156835be4
Byul <Byul@Gaming-PC>
2023-03-13 01:10:35 +0900

    now show result one by one

--- src/GetPresenter.java
+++ src/GetPresenter.java
@@ -1,45 +1,70 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
 public class GetPresenter {
 	public static void main(String[] args) {
-		new Roulettle();
+		new Roulettle().run();
 	}
 }
 class Roulettle {
-	List<String> def;
+	List<String> presenters;
 	List<String> volunteers;
 	Scanner sc;
 
 	Roulettle() {
-		this.def = new ArrayList<>(
-				Arrays.asList(
-					// names here 
-				)
-		);
+		this.presenters = new LinkedList<>(Arrays.asList("john", "david", "bob", "sarah", "mary", "kate", "steven", "jenny"
+		// names here
+		));
 		this.sc = new Scanner(System.in);
-		this.volunteers = new ArrayList<>();
+		this.volunteers = new LinkedList<>();
+		// ifVolunteers == 4 break;
+		// ifVolunteers > 4 run roulette with volunteers
+		// ifVolunteers < 4 check for exclude, set volunteers as presenters, remove
+		// volunteers from presenters and run roulette with presenters
+	}
+	void run() {
 		boolean isRun = true;
 		this.setVolunteers();
-		System.out.println("희망자 : " + this.volunteers);
+		System.out.println(this.volunteers.size() > 1 ? "희망자 : " + this.volunteers : "희망자 x");
 		if (this.volunteers.size() == 4)
 			isRun = false;
+		if (isRun)
+			this.updatePresenters();
 		if (this.volunteers.size() > 4 && isRun) {
-			System.out.println(this.runRoulette(true));
+			this.printResult(this.runRoulette(true));
 			isRun = false;
 		}
 		if (this.volunteers.size() < 4 && isRun) {
 			this.setExclude();
-			System.out.println(this.addLists(this.volunteers, this.runRoulette(false)));
+			this.printResult(this.addLists(this.volunteers, this.runRoulette(false)));
 		}
 		this.sc.close();
-		// ifVolunteers == 4 break;
-		// ifVolunteers > 4 run roulette with volunteers
-		// ifVolunteers < 4 check for exclude, set volunteers as presenters, remove
-		// volunteers from def and run roulette with def
+	}
+	void updatePresenters() {
+		System.out.println("발표 제외자 입력 (1. 종료)");
+		String s;
+		while (true) {
+			s = this.sc.nextLine().trim();
+			if (s.equals("1")) {
+				break;
+			}
+			for (int i = 0; i < this.presenters.size(); i++) {
+				if (this.presenters.get(i).contains(s)) {
+					this.presenters.remove(i);
+				}
+			}
+		}
+	}
+	void printResult(List<String> list) {
+		System.out.println("발표자 보기 enter");
+		for (String s : list) {
+			sc.nextLine();
+			System.out.println(s);
+		}
 	}
 	void setVolunteers() {
 		System.out.println("발표 희망자 입력 (1. 종료) ");
@@ -72,16 +97,16 @@ class Roulettle {
 		if (enoughVolunteers) {
 			list = this.volunteers;
 		} else {
-			list = this.def;
+			list = this.presenters;
 		}
 		Collections.shuffle(list);
-		return list.subList(0, enoughVolunteers ? 4 : 4 - this.volunteers.size());
+		return list.subList(0, enoughVolunteers ? 4 : list.size() > 3 ? 4 - this.volunteers.size() : list.size());
 	}
 	void setExclude() {
-		for (int i = 0; i < this.def.size(); i++) {
+		for (int i = 0; i < this.presenters.size(); i++) {
 			for (int j = 0; j < this.volunteers.size(); j++) {
-				if (this.def.get(i).contains(this.volunteers.get(j))) {
-					this.def.remove(i);
+				if (this.presenters.get(i).contains(this.volunteers.get(j))) {
+					this.presenters.remove(i);
 				}
 			}
 		}
